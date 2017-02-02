931a1ec3ec76739e56c53e808e23d8abae4a5dcc
Travis Hubbard <t3patterson@gmail.com>
2017-02-02 08:53:34 -0500

    files

---
+++ js/app.js
@@ -0,0 +1,56 @@
+//-------------------------------------
+//TASK : Put the songs on the page  
+//-------------------------------------
+//
+// Each song item needs to be in this format: 
+// 
+// <div class="song-item">
+// 	<h4 class="song-item__name">Song Title</h4>
+// 	<h4 class="song-item__artist">Artist</h4>
+// 	<h4 class="song-item__album">Album</h4>
+// </div> 
+
+// You will add them to the element with a class of 'songs-list'
+
+var jukeBoxSongs = [
+  {
+     artist: "Guns N Roses",
+     song: "Live & Let Die",
+     album: "Use Your Illusion"
+  },
+  {
+     artist: "Led Zeppelin",
+     song: "When the Levee Breaks",
+     album: "Led Zeppelein IV"
+  },
+  {
+     artist: "Guns N Roses",
+     song: "Live & Let Die",
+     album: "Use Your Illusion"
+  },
+  {
+     artist: "Eric Prydz",
+     song: "Call on Me",
+     album: "EP"
+  },
+  {
+      artist: "Nirvana",
+      song: "In Bloom",
+      album: "Nevermind"
+  },
+  {
+      artist: "Pearl Jam",
+      song: "Better Man",
+      album: "Vitalogy"
+  },
+  {
+     artist: "LCD Soundsystem",
+     song: "Dance Yrself Clean",
+     album: "This is Happening"
+  },
+  {
+     artist: "Spoon",
+     song: "Dont You Evah",
+     album: "Ga Ga Ga Ga Ga"
+  }
+ ]
---
+++ js/utils.js
@@ -0,0 +1,5 @@
+var forEach = function(arr, cb){
+	for(var i = 0; i < arr.length; i++){
+		cb(arr[i], i, arr)
+	}
+}
